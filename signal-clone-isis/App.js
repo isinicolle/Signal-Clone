dd8a1a20deb69c4ba4d594033ee7a64fc9de7554
Isis Zapata <isis.zapata.hn@gmail.com>
2022-07-10 14:47:30 -0600

    .
    .

--- signal-clone-isis/App.js
+++ signal-clone-isis/App.js
@@ -1,14 +1,21 @@
-import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
+import * as React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 import LoginScreen from './screens/LoginScreen';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-const Stack = createStackNavigator();
+const Stack = createNativeStackNavigator();
+
+const globalScreenOptions = {
+  headerStyle: { backgroundColor: '#2C6BED' },
+  headerTintColor: '#fff',
+  headerTitleStyle: { color: '#fff' },
+};
 
 export default function App() {
   return (
-    <NavigationContainer>
+    <NavigationContainer screenOptions={globalScreenOptions}>
     <Stack.Navigator>
       <Stack.Screen name='Login' component={LoginScreen} />
     </Stack.Navigator>
