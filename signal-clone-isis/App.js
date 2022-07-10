e4f51287ac2764c852e777749536bc3bdc952646
Isis Zapata <isis.zapata.hn@gmail.com>
2022-07-10 14:23:39 -0600

    yarn add react-native-screens react-native-safe-area-context
    yarn add react-native-screens react-native-safe-area-context

--- signal-clone-isis/App.js
+++ signal-clone-isis/App.js
@@ -2,14 +2,16 @@ import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import LoginScreen from './screens/LoginScreen';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Stack.Navigator>
+      <Stack.Screen name='Login' component={LoginScreen} />
+    </Stack.Navigator>
     </NavigationContainer> 
   );
 }
